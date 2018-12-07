0d0a5feb36d2ae1b6eeb91788c0d5aaed5d47851
Justin Wyatt <justinwyatt@Justins-MacBook-Pro.local>
2018-12-07 00:04:06 -0600

    added post and delete vue methods

--- client/src/components/PostComponent.vue
+++ client/src/components/PostComponent.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="container">
     <h1>Latest Posts</h1>
+    <div class="create-post">
+      <label for="create-post">Say Something...</label>
+      <input v-model="text" type="text" id="create-post" placeholder="Create a new post" />
+      <button v-on:click="createPost">Post</button>
+    </div>
     <hr>
     <p class="error" v-if="error">{{ error }}</p>
     <div class="posts-container">
@@ -8,7 +13,8 @@
            v-for="(post, index) in posts"
            v-bind:item="post"
            v-bind:index="index"
-           v-bind:key="post._id">
+           v-bind:key="post._id"
+           v-on:dblclick="deletePost(post._id)">
 
            {{ `${post.createdAt.getDate()}/${post.createdAt.getMonth()}/${post.createdAt.getFullYear()}`}}
 
@@ -37,6 +43,16 @@ export default {
       this.posts = await PostService.getPosts();
     }catch(err){
       this.error = err.message;
+    }
+  },
+  methods: {
+    async createPost() {
+      await PostService.insertPost(this.text);
+      this.posts = await PostService.getPosts();
+    },
+    async deletePost(id){
+      await PostService.deletePost(id);
+      this.posts = await PostService.getPosts();
     }
   }
 }
