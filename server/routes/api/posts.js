298441de7ca9ebff5e5449d709dded6637ef10e8
Justin Wyatt <justinwyatt@Justins-MacBook-Pro.local>
2018-12-06 23:04:27 -0600

    added post and delete routes

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -9,6 +9,23 @@ router.get('/', async (req, res) => {
   res.send(await posts.find({}).toArray());
 });
 
+router.post('/', async (req, res) => {
+  const posts = await loadPosts();
+
+  await posts.insertOne({
+    text: req.body.text,
+    createdAt: new Date()
+  });
+
+  res.status(201).send();
+});
+
+router.delete('/:id', async (req, res) => {
+  const posts = await loadPosts();
+  await posts.deleteOne({ _id: new mongodb.ObjectID(req.params.id) });
+  res.status(200).send();
+})
+
 
 async function loadPosts(){
   const client = await mongodb.MongoClient.connect('mongodb://abc123:abc123@ds127954.mlab.com:27954/vue-post', {
